18eb6b7f80d79f5140cd4f27e28b61609c38bd6d
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-05-14 15:58:40 +0000

    GA - 1405202501

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
+// pages/_app.js
+
 import 'react-notion-x/src/styles.css';
 import 'prismjs/themes/prism.css';        // code highlighting
 import "../styles/globals.css";
+
+import Script from "next/script";
+import { useEffect } from "react";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() {
+        window.dataLayer.push(arguments);
+      }
+      gtag("js", new Date());
+      gtag("config", "G-GZ4N5DCXE1"); 
+    }
+  }, []);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-GZ4N5DCXE1`}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
