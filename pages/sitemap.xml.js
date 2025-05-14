d519a0a2045e5691b54b023e98427bb69637ef09
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-05-14 16:15:25 +0000

    Sitemap - 1405202503

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,40 +1,39 @@
 // pages/sitemap.xml.js
+import { getPublishedBlogSlugs } from "../lib/notion";
 
 export async function getServerSideProps({ res }) {
-    const baseUrl = "https://www.bitftx.com";
-  
-    // Optional: Fetch blog posts from Notion or wherever needed
-    const blogSlugs = ["/blog/launch", "/blog/tokenomics", "/blog/airdrop"]; // Dynamically generate later
-  
-    const staticRoutes = ["/", "/tokenomics", "/airdrop", "/blog"];
-  
-    const allRoutes = [...staticRoutes, ...blogSlugs];
-  
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${allRoutes
-      .map(
-        (route) => `
-      <url>
-        <loc>${baseUrl}${route}</loc>
-        <changefreq>weekly</changefreq>
-        <priority>0.8</priority>
-      </url>
-    `
-      )
-      .join("")}
-  </urlset>`;
-  
-    res.setHeader("Content-Type", "text/xml");
-    res.write(sitemap);
-    res.end();
-  
-    return {
-      props: {},
-    };
-  }
-  
-  export default function Sitemap() {
-    return null;
-  }
-  
+  const baseUrl = "https://www.bitftx.com";
+
+  const staticRoutes = ["/", "/tokenomics", "/airdrop", "/blog"];
+
+  const blogSlugs = await getPublishedBlogSlugs();
+  const blogRoutes = blogSlugs.map((slug) => `/blog/${slug}`);
+
+  const allRoutes = [...staticRoutes, ...blogRoutes];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${allRoutes
+  .map(
+    (route) => `
+  <url>
+    <loc>${baseUrl}${route}</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`
+  )
+  .join("")}
+</urlset>`;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default function Sitemap() {
+  return null;
+}
