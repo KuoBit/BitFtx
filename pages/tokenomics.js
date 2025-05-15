4f359184058c2ee426407115bfdaa5bb1d4bf25a
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-05-15 13:20:09 +0000

    Tokenomics - 1505202509

--- pages/tokenomics.js
+++ pages/tokenomics.js
@@ -5,22 +5,24 @@ import TokenVesting from "@/components/TokenVesting";
 import TokenUtility from "@/components/TokenUtility";
 import FundUsage from "@/components/FundUsage";
 import TokenFAQ from "@/components/TokenFAQ";
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import PageWrapper from "@/components/PageWrapper"; // ✅ import wrapper
 
 export default function TokenomicsPage() {
   return (
     <>
-    <Header />
-      <TokenomicsHero />
-      <TokenOverview />
-      <TokenAllocation />
-      <TokenVesting />
-      <TokenUtility />
-      <FundUsage />
-      <TokenFAQ />
-    <Footer />
+      <Header />
+      <PageWrapper>
+        <TokenomicsHero />
+        <TokenOverview />
+        <TokenAllocation />
+        <TokenVesting />
+        <TokenUtility />
+        <FundUsage />
+        <TokenFAQ />
+      </PageWrapper>
+      <Footer />
     </>
   );
 }
