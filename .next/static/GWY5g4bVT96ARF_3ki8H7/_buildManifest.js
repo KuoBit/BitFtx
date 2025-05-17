9ea0924f7141b0d61f0e3237d8e1b9e9249d76eb
Ubuntu <ubuntu@ip-172-31-26-107.ec2.internal>
2025-05-17 14:38:16 +0000

    Telegram

--- .next/static/GWY5g4bVT96ARF_3ki8H7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/65-fff2d845b77ffb7e.js","static/chunks/pages/index-3bca850e1ceee6af.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
