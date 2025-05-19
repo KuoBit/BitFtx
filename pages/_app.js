6134d45bec360fa327d5bc8035c931692dc4a8b9
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-05-19 10:51:11 +0000

    AirdropModal - 1905202502

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,6 @@ import { createClient } from "@supabase/supabase-js";
 import Script from "next/script";
 import { useEffect } from "react";
 
-const supabase = createClient(
-  "https://onevirzsdrfxposewozx.supabase.co", // your actual URL
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZXZpcnpzZHJmeHBvc2V3b3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDIzNjksImV4cCI6MjA2MDM3ODM2OX0.IPFY8wqbxadZugoGIRWsGNU27tVqS8BEYJkem8WubAk" // your actual anon key
-);
-
 export default function Home() {
   const submitAirdrop = async (formData) => {
     return await supabase.from("airdrop_leads").insert([formData]);
