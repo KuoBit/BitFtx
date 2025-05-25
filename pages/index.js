76e03b3dc9dbc0f10998b24593945342f5acd8a7
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-05-25 10:51:21 +0000

    Referrer Module - 2505202513

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import { useRouter } from 'next/router';
 
 const supabase = createClient(
   "https://onevirzsdrfxposewozx.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZXZpcnpzZHJmeHBvc2V3b3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDIzNjksImV4cCI6MjA2MDM3ODM2OX0.IPFY8wqbxadZugoGIRWsGNU27tVqS8BEYJkem8WubAk"
 );
 
 const contractAddress = "0x42E6a5e559169b4cc5DEeB748795aE5F1970B221";
