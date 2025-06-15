81bceb4e6722aa55e3da52ecce6796fe58beab17
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-06-15 14:04:48 +0000

    Admin NAV - 1506202508

--- components/AdminHeader.js
+++ components/AdminHeader.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function AdminHeader() {
   return (
-    <nav className="space-x-6 text-sm text-white/80 block py-4 text-center">
+    <nav className="mt-4 space-x-6 text-sm text-white/80 block py-4 text-center">
       <Link href="/admin/airdrop-leads" className="hover:text-white font-medium text-purple-400">Airdrop Leads</Link>
       <Link href="/admin/airdrop-campaign" className="hover:text-white font-medium text-purple-400">Airdrop Campaign</Link>
       <Link href="/admin/withdrawals" className="hover:text-white font-medium text-purple-400">Withdrawal Requests</Link>
