885933f7deaa9955745865eff34ffb52bab8ef7d
KuoBit <98312436+KuoBit@users.noreply.github.com>
2025-08-02 13:32:32 +0000

    AdminMailer - 0208202503

--- pages/admin/AdminMailer.jsx
+++ pages/admin/AdminMailer.jsx
@@ -53,6 +53,7 @@ export default function AdminMailer() {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZXZpcnpzZHJmeHBvc2V3b3p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ4MDIzNjksImV4cCI6MjA2MDM3ODM2OX0.IPFY8wqbxadZugoGIRWsGNU27tVqS8BEYJkem8WubAk`, // Or hardcode your anon key if safe
           },
           body: JSON.stringify({
             subject,
